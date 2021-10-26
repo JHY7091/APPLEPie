495dbb9b61b9e1b449757ce216ecfa4e87cb8d2f
Guest <guest-rzvcpd@h20794.sgb.skir.kr>
2021-10-26 10:12:27 +0900

    second commit

---
+++ python_stepp1/python3.py
@@ -0,0 +1,28 @@
+#연산자
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) #제곱
+print(5%3) #나머지 구하기
+print(5//3) #몫
+
+print(10 > 3)
+print(4 >= 3)
+print(10 > 3)
+print(5 <= 5)
+print(3 == 3) #3 과 3 은 같다.
+print(3 + 4 == 7) # == 같다 라는 뜻이다.
+
+print(1 != 3) # !는 == 와 반대 즉 다르다 라는 뜻이다.
+print(not(1 != 3))
+
+print((3 > 0 ) and (3 < 5 )) #and 양쪽 다 충족한다면 True
+print((3 > 0) & (3 < 5)) # and == & 같다 
+
+print((3 > 0) or (3 > 5)) #or 양쪽 하나라도 충족한다면 True
+print((3 > 0) | (3 > 5)) #or == | 같다
+
+print(5 > 4 > 3)
+print(5 > 4 > 7)
